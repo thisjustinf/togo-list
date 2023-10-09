209fc7e3ca1db51d2a4336d0a58344bc7224dbd3
Justin Fagan <jbfagan7@gmail.com>
2023-10-08 21:04:28 -0400

    fix: modified gqlgen script

--- scripts/gqlgen.sh
+++ scripts/gqlgen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 printf "\nRegenerating gqlgen files\n"
-rm -f internal/graphql/generated/exec.go \
-    internal/graphql/models/generated.go \
-    internal/graphql/resolvers/generated/generated.go
+rm -f internal/graphql/generated/generated.go \
+    internal/graphql/models/models_gen.go
 time go run -v github.com/99designs/gqlgen $1
 printf "\nDone.\n\n"
